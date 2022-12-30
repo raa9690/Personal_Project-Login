d0c470e2c59e3b8e6b5b4cbbd83181d76e2ee4ed
raa9690 <raa9690@rit.edu>
2022-12-30 09:30:35 -0500

    Settup routing module

--- login-ui/src/app/app-routing.module.ts
+++ login-ui/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { HomePageComponent } from './home-page/home-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "login-page", component: LoginPageComponent},
+  {path: "home-page", component: HomePageComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

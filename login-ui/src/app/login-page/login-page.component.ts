03dcc725a70079229a7b437b13969884cbb5961f
raa9690 <raa9690@rit.edu>
2022-12-31 18:16:58 -0500

    loginAttempt success now redirects to homepage

--- login-ui/src/app/login-page/login-page.component.ts
+++ login-ui/src/app/login-page/login-page.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 
 import { LoginService } from '../login.service';
 
+import { Router, ActivatedRoute, ParamMap } from '@angular/router';
+
 @Component({
   selector: 'app-login-page',
   templateUrl: './login-page.component.html',
@@ -12,9 +14,15 @@ export class LoginPageComponent {
   // True at start so that the failed login message doesn't display until after a login attempt is made
   loginSuccess = true
 
-  constructor(private loginService: LoginService) { }
+  constructor(
+    private loginService: LoginService,
+    private router: Router
+    ) { }
 
   loginAttempt(username:string, password:string): boolean{
-    return this.loginService.loginAttempt(username, password);
+    if (this.loginService.loginAttempt(username, password)){
+      this.router.navigate(["home-page"])
+    }
+    return false;
   }
 }
